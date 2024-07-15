b0ca047a064e134250d1fd2b8ec84efad42fa84c
hrishikeshwarrier4 <warrier.h@northeastern.edu>
2024-07-15 18:14:27 -0400

    voters

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from _datetime import datetime
+import json
+from datetime import datetime
 import random
 
 import psycopg2
 import requests
-
-
+from confluent_kafka import SerializingProducer, KafkaException
+from confluent_kafka.serialization import StringSerializer
+from uuid import uuid4
 
 BASE_URL = 'https://randomuser.me/api/?nat=us'
-PARTIES = ['Unity Party','Liberty Alliance','Progressive Front','People\'s Choice Party']
+PARTIES = ['Unity Party', 'Liberty Alliance', 'Progressive Front', 'People\'s Choice Party']
 
 random.seed(43)
-
 
 # Sample data for dynamic fields
 EDUCATION_OPTIONS = [
@@ -43,6 +44,14 @@ CAMPAIGN_PLATFORM_OPTIONS = [
     "Environmental protection and renewable energy.",
     "Criminal justice reform.",
     "Strengthening local economies and small businesses."
+]
+
+POLLING_STATIONS = [
+    "Community Center Polling Station",
+    "High School Gym Polling Station",
+    "Library Polling Station",
+    "Town Hall Polling Station",
+    "Fire Station Polling Station"
 ]
 
 def create_tables(conn, cur):
@@ -82,7 +91,6 @@ def create_tables(conn, cur):
             polling_station VARCHAR(255)
         )
     """)
-
     cur.execute("""
         CREATE TABLE IF NOT EXISTS votes (
             voter_id VARCHAR(255) UNIQUE,
@@ -92,14 +100,12 @@ def create_tables(conn, cur):
             PRIMARY KEY (voter_id, candidate_id)
         )
     """)
-
     conn.commit()
 
 def generates_candidates(candidate_num, total_parties):
-    response = requests.get(BASE_URL + '&gender='+('male' if candidate_num %2 == 1 else 'female'))
+    response = requests.get(BASE_URL + '&gender=' + ('male' if candidate_num % 2 == 1 else 'female'))
     if response.status_code == 200:
         user_data = response.json()['results'][0]
-
         return {
             "candidate_id": user_data['login']['uuid'],
             "candidate_name": f"{user_data['name']['first']} {user_data['name']['last']}",
@@ -116,50 +122,127 @@ def generates_candidates(candidate_num, total_parties):
     else:
         return "Error fetching data"
 
+def insert_candidates(conn, cur, candidates):
+    try:
+        cur.execute("""
+            INSERT INTO candidates (candidate_id, candidate_name, date_of_birth, gender, party_affiliation, education, experience, biography, campaign_platform, campaign_funds,photo_url)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            candidates['candidate_id'], candidates['candidate_name'], candidates['date_of_birth'], candidates['gender'],
+            candidates['party_affiliation'],
+            candidates["education"], candidates['experience'], candidates['biography'],
+            candidates['campaign_platform'], candidates['campaign_funds'], candidates['photo_url']))
+        conn.commit()
+    except psycopg2.Error as e:
+        print(f"Error inserting candidate: {e}")
+        conn.rollback()
 
+def generates_voter():
+    response = requests.get(BASE_URL)
+    if response.status_code == 200:
+        user_data = response.json()['results'][0]
+        return {
+            "voter_id": user_data['login']['uuid'],
+            "voter_name": f"{user_data['name']['first']} {user_data['name']['last']}",
+            "date_of_birth": user_data['dob']['date'],
+            "gender": user_data['gender'],
+            "nationality": user_data['nat'],
+            "registration_number": user_data['login']['username'],
+            "address": {
+                "street": f"{user_data['location']['street']['number']} {user_data['location']['street']['name']}",
+                "city": user_data['location']['city'],
+                "state": user_data['location']['state'],
+                "country": user_data['location']['country'],
+                "postcode": user_data['location']['postcode']
+            },
+            "email": user_data['email'],
+            "phone_number": user_data['phone'],
+            "cell_number": user_data['cell'],
+            "picture": user_data['picture']['large'],
+            "registered_age": user_data['registered']['age'],
+            "polling_station": random.choice(POLLING_STATIONS)
+        }
+    else:
+        return "Error fetching data"
 
+def insert_voters(conn, cur, voter):
+    while True:
+        try:
+            cur.execute("""
+                INSERT INTO voters (voter_id, voter_name, date_of_birth, gender, nationality, registration_number, address_street, address_city, address_state, address_country, address_postcode, email, phone_number, cell_number, picture, registered_age, polling_station)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s)
+            """, (
+                voter["voter_id"], voter['voter_name'], voter['date_of_birth'], voter['gender'],
+                voter['nationality'], voter['registration_number'], voter['address']['street'],
+                voter['address']['city'], voter['address']['state'], voter['address']['country'],
+                voter['address']['postcode'], voter['email'], voter['phone_number'],
+                voter['cell_number'], voter['picture'], voter['registered_age'], voter['polling_station']
+            ))
+            conn.commit()
+            break
+        except psycopg2.errors.UniqueViolation:
+            voter["voter_id"] = str(uuid4())
+        except psycopg2.Error as e:
+            print(f"Error inserting voter: {e}")
+            conn.rollback()
+            break
 
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()}[{msg.partition()}]")
 
+if __name__ == '__main__':
+    producer_conf = {
+        'bootstrap.servers': 'localhost:9092',
+        'key.serializer': StringSerializer('utf_8'),
+        'value.serializer': StringSerializer('utf_8')
+    }
+    producer = SerializingProducer(producer_conf)
 
-if __name__=='__main__':
     try:
         conn = psycopg2.connect(
             dbname="election",
             user="postgres",
             password="postgres",
             host="localhost",
-            port="5433")
+            port="5433"
+        )
         cur = conn.cursor()
 
-
-        create_tables(conn,cur)
-
+        create_tables(conn, cur)
 
         cur.execute("""
             SELECT * FROM candidates
         """)
 
         candidates = cur.fetchall()
-        print(candidates)
 
-
-        if len(candidates)==0:
+        if len(candidates) == 0:
             for i in range(4):
-                candidates= generates_candidates(i,4)
-                print(candidates)
-                cur.execute("""
-                            INSERT INTO candidates (candidate_id, candidate_name, date_of_birth, gender, party_affiliation, education, experience, biography, campaign_platform, campaign_funds,photo_url)
-                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                        """, (
-                    candidates['candidate_id'], candidates['candidate_name'],candidates['date_of_birth'],candidates['gender'], candidates['party_affiliation'],
-                    candidates["education"], candidates['experience'],candidates['biography'],
-                    candidates['campaign_platform'],candidates['campaign_funds'], candidates['photo_url']))
-                conn.commit()
+                candidate = generates_candidates(i, 4)
+                insert_candidates(conn, cur, candidate)
+                print(candidate)
 
+        for i in range(4000):
+            voter = generates_voter()
+            if voter != "Error fetching data":
+                insert_voters(conn, cur, voter)
+                print(voter)
 
+                producer.produce(
+                    topic="voters_topic",
+                    key=voter['voter_id'],
+                    value=json.dumps(voter),
+                    on_delivery=delivery_report
+                )
+                producer.poll(0)
 
-
-
+        producer.flush()
 
     except Exception as e:
         print(e)
+    finally:
+        cur.close()
+        conn.close()
